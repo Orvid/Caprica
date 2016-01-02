058e1d2a20ffa632b92f6f0adfd5831f6a3997cf
Orvid King <blah38621@gmail.com>
2016-01-01 23:01:44 -0600

    Bump to version 0.0.3

--- Caprica/main.cpp
+++ Caprica/main.cpp
@@ -31,7 +31,7 @@ int main(int argc, char* argv[])
     printf("Invoke like: caprica.exe myFile.psc");
   }
 
-  printf("Caprica Papyrus Compiler v0.0.2\n");
+  printf("Caprica Papyrus Compiler v0.0.3\n");
 
 #ifdef NDEBUG
   try {
