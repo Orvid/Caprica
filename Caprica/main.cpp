948d55c179e7b9fea28c84d5b1fc2040120db730
Orvid King <blah38621@gmail.com>
2016-01-07 21:12:14 -0600

    Bump version number to v0.0.8

--- Caprica/main.cpp
+++ Caprica/main.cpp
@@ -70,7 +70,7 @@ int main(int argc, char* argv[])
     printf("Invoke like: caprica.exe myFile.psc");
   }
 
-  printf("Caprica Papyrus Compiler v0.0.7\n");
+  printf("Caprica Papyrus Compiler v0.0.8\n");
 
 #ifdef NDEBUG
   try {
