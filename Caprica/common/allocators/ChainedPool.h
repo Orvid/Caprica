746fa569cff2a7b8bbad891e94afa4ee5fb9d4e0
Orvid King <blah38621@gmail.com>
2016-07-09 15:21:06 -0700

    Add support for typed iteration of a chained pool.

--- Caprica/common/allocators/ChainedPool.h
+++ Caprica/common/allocators/ChainedPool.h
@@ -39,13 +39,16 @@ struct ChainedPool final
     }
     return new (buf + sizeof(DestructionNode)) T(std::forward<Args>(args)...);
   }
-  boost::string_ref ChainedPool::allocateString(std::string&& str);
+  boost::string_ref allocateString(std::string&& str);
   boost::string_ref allocateString(const char* str, size_t len);
 
   void reset();
   size_t totalAllocatedBytes() const { return totalSize; }
 
 private:
+  friend struct TypedHeapIteratorWrapper;
+  friend struct TypedHeapIterator;
+
   struct DestructionNode final
   {
     void(*destructor)(void*){ nullptr };
@@ -84,6 +87,81 @@ private:
     Heap* curHeap{ nullptr };
   };
 
+  template<typename T>
+  struct TypedHeapIterator final
+  {
+    size_t index{ 0 };
+
+    TypedHeapIterator<T>& operator ++() {
+      if (curHeap == nullptr)
+        return *this;
+      index++;
+      heapI++;
+      if (heapI < curHeap->baseAlloc + (curHeap->allocedHeapSize - curHeap->freeBytes))
+        return *this;
+      heapI = nullptr;
+      curHeap = curHeap->next;
+      if (curHeap) {
+        if (curHeap->freeBytes != curHeap->allocedHeapSize) {
+          heapI = (T*)curHeap->baseAlloc;
+        } else {
+          index = 0;
+          curHeap = nullptr;
+        }
+      }
+      return *this;
+    }
+
+    T& operator *() {
+      return *heapI;
+    }
+    const T& operator *() const {
+      return *heapI;
+    }
+    T& operator ->() {
+      return *heapI;
+    }
+    const T& operator ->() const {
+      return *heapI;
+    }
+
+    bool operator ==(const TypedHeapIterator<T>& other) const {
+      return curHeap == other.curHeap && heapI == other.heapI;
+    }
+
+    bool operator !=(const TypedHeapIterator<T>& other) const {
+      return !(this == other);
+    }
+
+  private:
+    friend TypedHeapIteratorWrapper;
+
+    Heap* curHeap{ nullptr };
+    T* heapI{ nullptr };
+
+    TypedHeapIterator() = default;
+    TypedHeapIterator(Heap* cur) : curHeap(cur) { }
+  };
+
+  template<typename T>
+  struct TypedHeapIteratorWrapper final
+  {
+    TypedHeapIterator<T> begin() const {
+      if (!parent->totalSize)
+        return TypedHeapIterator<T>();
+      return TypedHeapIterator<T>(&parent->base);
+    }
+
+    TypedHeapIterator<T> end() const {
+      return TypedHeapIterator<T>();
+    }
+
+  private:
+    friend ChainedPool;
+    ChainedPool* parent;
+    TypedHeapIteratorWrapper(ChainedPool* p) : parent(p) { }
+  };
+
   size_t heapSize;
   size_t totalSize{ 0 };
   Heap* current{ &base };
@@ -96,6 +174,11 @@ private:
 public:
   HeapIterator begin() const;
   HeapIterator end() const;
+
+  template<typename T>
+  TypedHeapIteratorWrapper<T> typedIterator() {
+    return TypedHeapIteratorWrapper<T>(this);
+  }
 };
 
 }}
