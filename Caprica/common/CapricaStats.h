e5569c7669f08665d662b5f69411e52172283339
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-10-13 21:02:52 -0700

    fix stat reporting

--- Caprica/common/CapricaStats.h
+++ Caprica/common/CapricaStats.h
@@ -9,28 +9,22 @@ private:
   struct NopIncStruct final {
     size_t val;
 
-    NopIncStruct& operator++(int) {
-      val++;
-      return *this;
-    }
-    NopIncStruct& operator=(size_t f) {
-      val = f;
-      return *this;
-    }
+    NopIncStruct& operator++(int) { return *this; }
+    NopIncStruct& operator=(size_t) { return *this; }
   };
 
   // using counter_type = std::atomic<size_t>;
-  // using counter_type = size_t;
-  using counter_type = NopIncStruct;
+  using counter_type = size_t;
+  // using counter_type = NopIncStruct;
 
 public:
-  static counter_type peekedTokenCount;
-  static counter_type consumedTokenCount;
+  static NopIncStruct peekedTokenCount;
+  static NopIncStruct consumedTokenCount;
   static counter_type importedFileCount;
   static counter_type inputFileCount;
-  static counter_type lexedFilesCount;
-  static counter_type allocatedHeapCount;
-  static counter_type freedHeapCount;
+  static NopIncStruct lexedFilesCount;
+  static NopIncStruct allocatedHeapCount;
+  static NopIncStruct freedHeapCount;
 
   static void outputStats();
   static void outputImportedCount();
