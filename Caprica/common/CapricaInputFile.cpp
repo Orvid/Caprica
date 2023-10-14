ee8cebbf70f20bdc4de84d7c63bd7c1faf19c394
nikitalita <69168929+nikitalita@users.noreply.github.com>
2023-10-13 19:20:11 -0700

    cleanup caprica input file

--- Caprica/common/CapricaInputFile.cpp
+++ Caprica/common/CapricaInputFile.cpp
@@ -1,198 +1,181 @@
-#include <filesystem>
+#include "CapricaInputFile.h"
+#include <common/CapricaConfig.h>
 #include <common/CapricaInputFile.h>
 #include <common/FSUtils.h>
 #include <common/parser/PapyrusProject.h>
-#include <common/CapricaConfig.h>
-#include "CapricaInputFile.h"
+#include <filesystem>
 
-namespace caprica{
+namespace caprica {
 
-  std::filesystem::path InputFile::resolved_relative() const {
-    if (!resolved) return {};
-    return absPath.lexically_relative(absBaseDir);
-  }
-
-  std::filesystem::path InputFile::resolved_absolute() const {
-    if (!resolved) return {};
-    return absPath; 
-  }
-
-  std::filesystem::path InputFile::resolved_absolute_basedir() const {
-    if (!resolved) return {};
-    return absBaseDir;
-  }
-
-  std::filesystem::path IInputFile::find_import_dir(const std::filesystem::path& path) {
-    for (auto& dir : conf::Papyrus::importDirectories) {
-      if (dirContains(path, dir.resolved_absolute())) {
-        return dir.resolved_absolute();
-      }
-    }
+std::filesystem::path IInputFile::resolved_relative() const {
+  if (!resolved)
     return {};
-  }
+  auto rel = absPath.lexically_relative(absBaseDir);
+  if (rel == ".")
+    return {};
+  return rel;
+}
 
-  bool IInputFile::dirContains(const std::filesystem::path& path, const std::filesystem::path& dir) {
-    if (path.is_absolute()) {
-      // check if the path is contained in the import directory
-      auto rel = path.lexically_relative(dir).string();
-      if (!rel.empty() && !rel.starts_with(".."))
-        return true;
-    } else {
-      if (std::filesystem::exists(dir / path))
-        return true;
-    }
+std::filesystem::path IInputFile::resolved_absolute() const {
+  if (!resolved)
+    return {};
+  return absPath;
+}
+
+std::filesystem::path IInputFile::resolved_absolute_basedir() const {
+  if (!resolved)
+    return {};
+  return absBaseDir;
+}
+
+std::filesystem::path IInputFile::resolved_relative_parent() const {
+  if (!resolved)
+    return {};
+  auto rel = resolved_relative();
+  if (rel.empty())
+    return {};
+  return rel.parent_path();
+}
+
+std::filesystem::path IInputFile::find_import_dir(const std::filesystem::path& path) {
+  for (auto& dir : conf::Papyrus::importDirectories)
+    if (dirContains(path, dir.resolved_absolute()))
+      return dir.resolved_absolute();
+  return {};
+}
+
+bool IInputFile::dirContains(const std::filesystem::path& path, const std::filesystem::path& dir) {
+  if (path.is_absolute()) {
+    // check if the path is contained in the import directory
+    auto rel = path.lexically_relative(dir).string();
+    if (!rel.empty() && !rel.starts_with(".."))
+      return true;
+  } else {
+    if (std::filesystem::exists(dir / path))
+      return true;
+  }
+  return false;
+}
+
+IInputFile::IInputFile(const std::filesystem::path& _path, bool noRecurse, const std::filesystem::path& _cwd)
+    : noRecurse(noRecurse),
+      rawPath(std::move(_path)),
+      cwd(_cwd.empty() ? std::filesystem::current_path() : std::move(FSUtils::canonicalFS(_cwd))) {
+}
+
+bool IInputFile::exists() const {
+  if (!resolved)
     return false;
-  }
+  return std::filesystem::exists(resolved_absolute());
+}
 
-  IInputFile::IInputFile(const std::filesystem::path& _path, bool noRecurse, const std::filesystem::path& _cwd)
-      : noRecurse(noRecurse),
-        rawPath(std::move(_path)),
-        cwd(_cwd.empty() ? std::filesystem::current_path() : std::move(FSUtils::canonicalFS(_cwd))) {
-  }
+bool IInputFile::isDir() const {
+  return std::filesystem::is_directory(resolved_absolute());
+}
 
-  bool InputFile::exists() const {
-    return std::filesystem::exists(resolved_absolute());
-  }
-
-  bool InputFile::isDir() const {
-    return std::filesystem::is_directory(resolved_absolute());
-  }
-
-  bool InputFile::resolve() {
-    auto normalPath = FSUtils::normalize(rawPath);
-    auto str = normalPath.string();
-    if (!normalPath.is_absolute()) {
-      absPath = FSUtils::canonicalFS(cwd / normalPath);
-      absBaseDir = cwd;
-      if (std::filesystem::exists(absPath)) {
-        resolved = true;
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      absPath = FSUtils::canonicalFS(normalPath);
-      absBaseDir = find_import_dir(absPath);
-      if (absBaseDir.empty()) {
-        absBaseDir = absPath.parent_path();
-        requiresPreParse = true;
-      }
-    }
-
+bool InputFile::resolve() {
+  auto normalPath = FSUtils::normalize(rawPath);
+  auto str = normalPath.string();
+  if (!normalPath.is_absolute()) {
+    absPath = FSUtils::canonicalFS(cwd / normalPath);
+    absBaseDir = cwd;
     if (std::filesystem::exists(absPath)) {
       resolved = true;
       return true;
-    }
-
-    return false;
-  }
-
-  InputFile::InputFile(const std::filesystem::path& _path, bool noRecurse, const std::filesystem::path& _cwd): IInputFile(_path, noRecurse, _cwd) {
-  }
-
-  ImportDir::ImportDir(const std::filesystem::path& _path, bool noRecurse, const std::filesystem::path& _cwd)
-      : IInputFile(_path, noRecurse, _cwd) {
-    import = true;
-    resolve(); // we resolve import dirs immediately
-  }
-
-  std::filesystem::path ImportDir::resolved_relative() const {
-    return {};
-  }
-
-  std::filesystem::path ImportDir::resolved_absolute() const {
-    return resolvedPath; // we always return the absolute path for imports
-  }
-
-  std::filesystem::path ImportDir::resolved_absolute_basedir() const {
-    return resolvedPath;
-  }
-
-  bool ImportDir::exists() const {
-    return std::filesystem::exists(resolvedPath);
-  }
-
-  bool ImportDir::resolve() {
-    if (!rawPath.is_absolute())
-      resolvedPath = FSUtils::canonicalFS(cwd / rawPath);
-    else
-      resolvedPath = FSUtils::canonicalFS(rawPath);
-
-    if (std::filesystem::exists(resolvedPath) && std::filesystem::is_directory(resolvedPath)) {
-      resolved = true;
-      return true;
-    }
-    return false;
-  }
-
-  PCompInputFile::PCompInputFile(const std::filesystem::path& _path,
-                                          bool noRecurse,
-                                          bool isFolder,
-                                          const std::filesystem::path& _cwd): IInputFile(_path, noRecurse, _cwd) {
-    __isFolder = isFolder;
-  }
-
-  std::filesystem::path PCompInputFile::resolved_relative() const {
-    if (!resolved) return {};
-    return absPath.lexically_relative(absBaseDir);
-  }
-
-  std::filesystem::path PCompInputFile::resolved_absolute() const {
-    if (!resolved) return {};
-    return absPath;
-  }
-
-  std::filesystem::path PCompInputFile::resolved_absolute_basedir() const {
-    if (!resolved) return {};
-    return absBaseDir;
-  }
-
-  bool PCompInputFile::exists() const {
-    return std::filesystem::exists(resolved_absolute());
-  }
-  
-  static constexpr char const curDir[3] = {'.', FSUtils::SEP, 0};
-  static constexpr char const parent[4] = {'.', '.', FSUtils::SEP, 0};
-
-  bool PCompInputFile::resolve() {
-    std::filesystem::path normalPath = FSUtils::objectNameToPath(rawPath.string());
-    if (!__isFolder && normalPath.extension().empty())
-      normalPath.replace_extension(".psc");
-    normalPath = FSUtils::normalize(normalPath);
-    std::string str = normalPath.string();
-    // special case for relative paths that contain parent/cwd refs
-    if (!normalPath.is_absolute() && (str == "." || str == ".." || str.starts_with(curDir) || str.contains(parent))) {
-      absPath = FSUtils::canonicalFS(cwd / normalPath);
-      absBaseDir = cwd;
-      
-      if (!std::filesystem::exists(absPath))
-        return false;
-      resolved = true;
-      return true;
-    }
-
-    // if this is a relative folder path, and the folder is in the cwd, use cwd as the base dir
-    if (__isFolder && !normalPath.is_absolute() && dirContains(normalPath, cwd)) {
-      absBaseDir = cwd;
     } else {
-      absBaseDir = find_import_dir(normalPath); 
-    }
-
-    if (absBaseDir.empty()) {
       return false;
     }
-
-    if (!normalPath.is_absolute()) {
-      absPath = FSUtils::canonicalFS(absBaseDir / normalPath);
-    } else {
-      absPath = FSUtils::canonicalFS(normalPath);
+  } else {
+    absPath = FSUtils::canonicalFS(normalPath);
+    absBaseDir = find_import_dir(absPath);
+    if (absBaseDir.empty()) {
+      absBaseDir = absPath.parent_path();
+      requiresPreParse = true;
     }
-  
-    if (std::filesystem::exists(absPath)) {
-      resolved = true;
-      return true;
-    }
-    return false;
   }
+
+  if (std::filesystem::exists(absPath)) {
+    resolved = true;
+    return true;
+  }
+
+  return false;
+}
+
+InputFile::InputFile(const std::filesystem::path& _path, bool noRecurse, const std::filesystem::path& _cwd)
+    : IInputFile(_path, noRecurse, _cwd) {
+  requiresPreParse = true; // we always require pre-parse for non-PCompiler-compatible input files
+}
+
+ImportDir::ImportDir(const std::filesystem::path& _path, bool noRecurse, const std::filesystem::path& _cwd)
+    : IInputFile(_path, noRecurse, _cwd) {
+  requiresPreParse = true; // we always require pre-parse for import dirs
+  import = true;
+  resolve(); // we resolve import dirs immediately
+}
+
+bool ImportDir::resolve() {
+  if (!rawPath.is_absolute())
+    absPath = FSUtils::canonicalFS(cwd / rawPath);
+  else
+    absPath = FSUtils::canonicalFS(rawPath);
+  absBaseDir = absPath;
+  if (std::filesystem::exists(absPath) && std::filesystem::is_directory(absPath)) {
+    resolved = true;
+    return true;
+  }
+  return false;
+}
+
+PCompInputFile::PCompInputFile(const std::filesystem::path& _path,
+                               bool noRecurse,
+                               bool isFolder,
+                               const std::filesystem::path& _cwd)
+    : IInputFile(_path, noRecurse, _cwd) {
+  __isFolder = isFolder;
+}
+
+static constexpr char const curDir[3] = { '.', FSUtils::SEP, 0 };
+static constexpr char const parent[4] = { '.', '.', FSUtils::SEP, 0 };
+
+bool PCompInputFile::resolve() {
+  std::filesystem::path normalPath = FSUtils::objectNameToPath(rawPath.string());
+  if (!__isFolder && normalPath.extension().empty())
+    normalPath.replace_extension(".psc");
+  normalPath = FSUtils::normalize(normalPath);
+  std::string str = normalPath.string();
+  // special case for relative paths that contain parent/cwd refs
+  if (!normalPath.is_absolute() && (str == "." || str == ".." || str.starts_with(curDir) || str.contains(parent))) {
+    absPath = FSUtils::canonicalFS(cwd / normalPath);
+    absBaseDir = cwd;
+
+    if (!std::filesystem::exists(absPath))
+      return false;
+    resolved = true;
+    return true;
+  }
+
+  // if this is a relative folder path, and the folder is in the cwd, use cwd as the base dir
+  if (__isFolder && !normalPath.is_absolute() && dirContains(normalPath, cwd))
+    absBaseDir = cwd;
+  else
+    absBaseDir = find_import_dir(normalPath);
+
+  if (absBaseDir.empty())
+    return false;
+
+  if (!normalPath.is_absolute())
+    absPath = FSUtils::canonicalFS(absBaseDir / normalPath);
+  else
+    absPath = FSUtils::canonicalFS(normalPath);
+
+  if (!std::filesystem::exists(absPath))
+    return false;
+  if (__isFolder && !std::filesystem::is_directory(absPath))
+    return false;
+
+  resolved = true;
+  return true;
+}
 
 } // namespace caprica
--- Caprica/common/CapricaInputFile.h
+++ Caprica/common/CapricaInputFile.h
@@ -1,80 +1,60 @@
 #pragma once
 #include <filesystem>
-namespace caprica{
+namespace caprica {
 struct IInputFile;
 struct IInputFile {
-  virtual std::filesystem::path resolved_relative() const = 0;
-  virtual std::filesystem::path resolved_absolute() const = 0;
-  virtual std::filesystem::path resolved_absolute_basedir() const = 0;
+  std::filesystem::path resolved_relative() const;
+  std::filesystem::path resolved_absolute() const;
+  std::filesystem::path resolved_absolute_basedir() const;
+  std::filesystem::path resolved_relative_parent() const;
   std::filesystem::path get_unresolved_path() const { return rawPath; }
   bool isRecursive() const { return !noRecurse; }
   bool isImport() const { return import; }
-  virtual bool exists() const = 0;
-  virtual bool isDir() const = 0;
+  bool isResolved() const { return resolved; }
+  virtual bool exists() const;
+  virtual bool isDir() const;
   virtual bool resolve() = 0;
-  virtual bool isResolved() const { return resolved; }
-  IInputFile(const std::filesystem::path& _path,
-            bool noRecurse = true,
-            const std::filesystem::path& _cwd = "");
+  IInputFile(const std::filesystem::path& _path, bool noRecurse = true, const std::filesystem::path& _cwd = "");
   virtual ~IInputFile() = default;
 
-  // static InputFile createNewStyle(const std::filesystem::path& path, bool noRecurse = true, const std::filesystem::path& cwd = std::filesystem::current_path());
-  // static InputFile createOldStyle(const std::filesystem::path& path, bool noRecurse = true, const std::filesystem::path& cwd = std::filesystem::current_path(), bool isFolder = false);
 protected:
   bool noRecurse = true;
   const std::filesystem::path rawPath;
   const std::filesystem::path cwd;
+  std::filesystem::path absPath;
+  std::filesystem::path absBaseDir; // import dir
+  static std::filesystem::path find_import_dir(const std::filesystem::path& _path);
+  static bool dirContains(const std::filesystem::path& _path, const std::filesystem::path& dir);
+
   bool resolved = false;
   bool import = false;
   bool requiresPreParse = false;
-  static std::filesystem::path find_import_dir(const std::filesystem::path& _path);
-  static bool dirContains(const std::filesystem::path& _path, const std::filesystem::path& dir);
 };
 
 struct InputFile : public IInputFile {
-  virtual std::filesystem::path resolved_relative() const override;
-  virtual std::filesystem::path resolved_absolute() const override;
-  virtual std::filesystem::path resolved_absolute_basedir() const override;
-  virtual bool exists() const override;
-  virtual bool isDir() const override;
   virtual bool resolve() override;
-  InputFile(const std::filesystem::path& _path,
-            bool noRecurse = true,
-            const std::filesystem::path& _cwd = "");
+  InputFile(const std::filesystem::path& _path, bool noRecurse = true, const std::filesystem::path& _cwd = "");
   virtual ~InputFile() = default;
-
-  private:
-    std::filesystem::path absPath;
-    std::filesystem::path absBaseDir;
 };
 
 struct PCompInputFile : public IInputFile {
   PCompInputFile(const std::filesystem::path& _path,
-             bool noRecurse = true, bool isFolder = false,
-             const std::filesystem::path& _cwd = "");
-  virtual std::filesystem::path resolved_relative() const override;
-  virtual std::filesystem::path resolved_absolute() const override;
-  virtual std::filesystem::path resolved_absolute_basedir() const override;
-  virtual bool exists() const override;
+                 bool noRecurse = true,
+                 bool isFolder = false,
+                 const std::filesystem::path& _cwd = "");
   virtual bool isDir() const override { return __isFolder; }
   virtual bool resolve() override;
-  private:
-    bool __isFolder = false;
-    std::filesystem::path absPath;
-    std::filesystem::path absBaseDir;
+
+private:
+  bool __isFolder = false;
 };
 
 struct ImportDir : public IInputFile {
-  ImportDir(const std::filesystem::path& _path,
-             bool noRecurse = true,
-             const std::filesystem::path& _cwd = "");
-  virtual std::filesystem::path resolved_relative() const override;
-  virtual std::filesystem::path resolved_absolute() const override;
-  virtual std::filesystem::path resolved_absolute_basedir() const override;
-  virtual bool exists() const override;
+  ImportDir(const std::filesystem::path& _path, bool noRecurse = true, const std::filesystem::path& _cwd = "");
   virtual bool isDir() const override { return true; }
   virtual bool resolve() override;
-  private:
-    std::filesystem::path resolvedPath;
+
+private:
+  std::filesystem::path resolvedPath;
 };
 } // namespace caprica
